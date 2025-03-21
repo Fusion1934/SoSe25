474d1ad24a2c37fbede0fb533f5adab013694525
Fusion 1934 <fusion1934@icloud.com>
2025-03-21 09:01:42 +0100

    Added to test

--- Test.java
+++ Test.java
@@ -1,5 +1,12 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        test();
     }
+
+    public static void test() {
+        System.out.println("Hello, World!");
+    }
+
+
 }
